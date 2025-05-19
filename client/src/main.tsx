b5816de35af2a67ada253687a01221c23b6355a7
samlik shan <samlikshanm@gmail.com>
2025-05-19 16:47:49 +0530

    Fix: remove react-chartjs and align chart.js with react-chartjs-2

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { persistor, store } from "./store/store.ts";
+import { persistor, store } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
